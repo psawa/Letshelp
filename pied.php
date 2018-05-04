ce735a6bb80357830f92645bee5d7ab30e4feee8
pplaut <38976707+pplaut@users.noreply.github.com>
2018-05-04 11:46:43 +0200

    Update pied.php

--- pied.php
+++ pied.php
@@ -4,6 +4,7 @@
 
 		<footer>
 			<p> Merci pour votre visite. Let's Help, 2018 </p>
+			<p> Ce site a été crée par Annaelle Mounié, Pauline Pasquiet et Thibo Rosemplatt <p>
 		</footer>
 	</div>	
 </body>
