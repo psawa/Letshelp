9eb5d66e4335ba530f700ab28bc734f864e48454
pplaut <38976707+pplaut@users.noreply.github.com>
2018-05-13 17:54:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ include('entete.php'); ?>
 <h2>Bienvenue sur <span style="font-family: 'Boogaloo', cursive;">Let's Help</span> ;) !</h2>
 
 <p>
-	Ce site est un site d\'entraide coopératif. tu peux <a href="poster_annonce1.php">chercher / proposer de l'aide</a>, <a href="voir_annonces.php">voir les annonces en cours</a>, et <a href="cpasmafaute.php">trouver/poster des excuses</a>.<br/><br/>
-	Pour poster une annonce ou une excuse, tu dois être inscrit. En revanche, la consultation est libre. <br/><br/> 
-	Pour plus d'informations n'hésite par à aller sur la page <a href="a_propos.php">A propos</a>. 
+	Ce site est un site d'entraide coopératif. tu peux <a href="poster_annonce1.php"> chercher / proposer de l'aide</a>, <a href="voir_annonces.php"> voir les annonces en cours </a>, et <a href="cpasmafaute.php"> trouver/poster des excuses </a>.<br/><br/>
+	Pour poster une annonce ou une excuse, tu dois être inscrit.e. En revanche, la consultation est libre. <br/><br/> 
+	Pour plus d'informations, n'hésite par à aller sur la page <a href="a_propos.php"> A propos </a>. 
 </p>
 
 <?php include('pied.php') ?>
