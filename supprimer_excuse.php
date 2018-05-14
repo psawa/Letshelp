3c1391f2f662aa46926138d4593ab8ca60bf2edf
pplaut <38976707+pplaut@users.noreply.github.com>
2018-05-14 16:22:33 +0200

    Update supprimer_excuse.php

--- supprimer_excuse.php
+++ supprimer_excuse.php
@@ -7,7 +7,7 @@ include 'entete.php'; ?>
 <?php 
 	if(isset($_SESSION['membre_id']) AND $_SESSION['membre_id']>0){ ?>
 	<?php 
-	$requete = "SELECT cpasmafaute.id_membre FROM cpasmafaute WHERE cpasmafaute.id=?;";
+	$requete = "SELECT excuse.id_membre FROM excuse WHERE excuse.id=?;";
 	$reponse=$pdo->prepare($requete);
 	$reponse->execute(array($_GET['id']));
 	$enregistrements = $reponse->fetch();
@@ -18,7 +18,7 @@ include 'entete.php'; ?>
 <?php if(isset($_GET['id'])AND $_SESSION['membre_id']==$membre_id){ ?>
 	
 	<?php 
-	$requete="UPDATE cpasmafaute SET active = 0 WHERE cpasmafaute.id = ?;";
+	$requete="UPDATE excuse SET active = 0 WHERE excuse.id = ?;";
 	$reponse=$pdo->prepare($requete);
 	$reponse->execute(array($_GET['id']));
 	$reponse->execute();
@@ -32,7 +32,7 @@ else { ?>
 ?>	
 
 <?php 
-$requete="SELECT active FROM cpasmafaute WHERE cpasmafaute.id=?;";
+$requete="SELECT active FROM excuse WHERE excuse.id=?;";
 $reponse=$pdo->prepare($requete);
 $reponse->execute(array($_GET['id']));
 $reponse->execute();
