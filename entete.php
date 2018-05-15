ca3321797a1897ee5e3d997f31980d7f479146cd
pplaut <38976707+pplaut@users.noreply.github.com>
2018-05-15 17:15:15 +0200

    Update entete.php

--- entete.php
+++ entete.php
@@ -7,6 +7,7 @@
 	<meta charset="utf-8">
 	<title>Let's Help - <?php if(isset($donnees['titre_page'])){echo $donnees['titre_page'];} else{echo "Page inconnue";} ?></title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="media_queries.css">
 	<!-- On importe des polices via google font -->
 	<style>
 		@import url('https://fonts.googleapis.com/css?family=Boogaloo|Indie+Flower');
