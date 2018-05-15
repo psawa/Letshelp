620413af17e96d92e4eea4fdd1da2d97af64aeca
upsawa <mkbloody@gmail.com>
2018-05-15 09:45:07 +0200

    Double confirmation mdp

--- enregistrer_membre.php
+++ enregistrer_membre.php
@@ -5,8 +5,9 @@ include 'entete.php'; ?>
 
 	
 	<?php 
-	if($_POST['pseudo']!=''AND $_POST['email']!='' AND $_POST['motdepasse']!=''){ //on vériie que les champs sont remplis
+	if($_POST['pseudo']!=''AND $_POST['email']!='' AND $_POST['motdepasse']!='' AND $_POST['motdepasse_confirm'] != ''){ //on vériie que les champs sont remplis
 		$motdepasse = $_POST['motdepasse'];
+		$motdepasse_confirm = $_POST['motdepasse_confirm'];
 		$pseudo = $_POST['pseudo'];
 		$email = $_POST['email'];
 		//requete qui prend seulement le pseudo s'il existe
@@ -15,6 +16,13 @@ include 'entete.php'; ?>
 		$reponse1 -> execute(array($pseudo));
 		$enregistrements1 = $reponse1->fetchAll();
 		$nombre_enregistrements1 = count($enregistrements1);
+
+		// requete qui prend l'email seulement s'il existe
+		$requete2 = "SELECT email FROM membre WHERE email = ?;";
+		$reponse2 = $pdo->prepare($requete2);
+		$reponse2 -> execute(array($email));
+		$enregistrements2 = $reponse2->fetchAll();
+		$nombre_enregistrements2 = count($enregistrements2);
 	}
 	else{ ?>
 		<p>Vous n'avez pas renseigné tous les champs. Veuillez recommencer.<p>
@@ -23,19 +31,37 @@ include 'entete.php'; ?>
 	// Si le pseudo existe déjà
 	if($nombre_enregistrements1>0){
 		?>
-		<p>Désolé ! ce pseudo est déjà utilisé.</p>
+		<p>Désolé ! ce pseudo est déjà utilisé.</p><br/>
+		<p><a href="inscription.php">Réessayer</a></p>
 	<?php 
 	}
+	elseif(count($enregistrements2)>0){?>
+		<p>Désolé, cet email est déjà utilisé.</p><br/>
+		<p><a href="inscription.php">Réessayer</a></p>
+	<?php
+	}
 	else{
-		$requete2 = "INSERT INTO membre (pseudo, mot_de_passe, email) VALUES (?, md5(?), ?);";
-		$reponse2 = $pdo->prepare($requete2);
-		$reponse2->execute(array($pseudo, $motdepasse, $email)); ?>
+		if($motdepasse==$motdepasse_confirm){
+			$requete2 = "INSERT INTO membre (pseudo, mot_de_passe, email) VALUES (?, md5(?), ?);";
+			$reponse2 = $pdo->prepare($requete2);
+			$reponse2->execute(array($pseudo, $motdepasse, $email)); ?>
 
-		<p>Bravo, vous êtes bien inscrits. Vous pouvez maintenant vous <a href="connexion.php">connecter</a>.</p>
-		<h2>Récap : </h2>
-		<p>Votre pseudo : <?php echo $pseudo; ?> <br></p>
-		<p>Votre email : <?php echo $email; ?> <br></p>
-		<?php 
+			<p>Bravo, tu es bien inscrit. Nous t'avons envoyé tes informations par email. <br/> Tu peux maintenant te <a href="connexion.php">connecter</a>.</p>
+			<h2>Récap : </h2>
+			<p>Votre pseudo : <?php echo $pseudo; ?> <br></p>
+			<p>Votre email : <?php echo $email; ?> <br></p>
+			<?php 
+
+			$mailto = $email;
+			$sujet = "Confirmation d'inscription sur Let's Help";
+			$txt = "Tu es bien inscrit sur Let's Help. \n\n Ton pseudo : ".$pseudo."\n Ton email : ".$email."\n\n À très bientôt ! ";
+			$headers = ("From: contact@letshelp.fr ");
+
+			mail($mailto,$sujet, $txt, $headers);
+		}
+		else{
+			echo "<p>Vos mot de passes ne correspondent pas. Veuillez <a href='inscription.php'>réessayer</a>.</p>";
+		}
 	}
 		?>
 
