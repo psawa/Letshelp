8c2412f495e13844c9c13fb4ecd87f6f6279e88f
pplaut <38976707+pplaut@users.noreply.github.com>
2018-05-15 14:01:35 +0200

    Create CSS_media_queries.php

---
+++ CSS_media_queries.php
@@ -0,0 +1,30 @@
+/* media queries*/
+
+@media screen and (max-width: 640px) 
+  {
+    .loremipsum {
+    background-color : #FFFFFF;
+    }
+  }
+
+@media screen and (min-width: 641px) 
+  {
+    .loremipsum {
+    background-color : #CCCCCC;
+    }
+  }
+
+/* max-width pour les petits ´ecrans */
+@media screen and (max-width: 640px) 
+  {
+    #coteacote
+      {
+      display: block;
+      }
+    section, aside
+      {
+      width: auto;
+      }
+  }
+  
+  
