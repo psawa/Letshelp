e99b6711b7a23c4e8d9190b3ddd5290e1ba7e41d
AnnaelleM <38976701+AnnaelleM@users.noreply.github.com>
2018-05-15 12:25:16 +0200

    Add files via upload

--- modifier_mdp.php
+++ modifier_mdp.php
@@ -1,6 +1,62 @@
 <?php 
-$donnees['menu'] ='';
-$donnees['titre_page']='Modifier mon mot de passe';
+$donnees['menu'] ='mon_compte';
+$donnees['titre_page']='Mon compte';
 include 'entete.php'; ?>
+
+
+<?php if(isset($_SESSION['membre_id']) AND $_SESSION['membre_id']>0): ?>
+
+<h2> Modifier Mot de Passe </h2><br>
+
+
+<?php 
+	if(isset($_POST['nouveau_mdp'])): ?>
+	<?php 
+
+	$motdepasse_confirm = $_POST['motdepasse_confirm'];
+	$nouveau_mdp = $_POST['nouveau_mdp'];
+
+	$requete = 'SELECT * FROM membre WHERE pseudo= ?;';
+	$reponse = $pdo->prepare($requete);
+	$reponse -> execute(array($_SESSION['pseudo']));
+
+	$enregistrements = $reponse->fetchAll();
+
+	
+		if($enregistrements[0]['mot_de_passe'] == md5($_POST['motdepasse'])){//on vérifie que le mdp est bon
+			if($nouveau_mdp==$motdepasse_confirm){
+			$requete="UPDATE membre SET mot_de_passe = md5(?) WHERE membre.id = ?;";
+			$reponse=$pdo->prepare($requete);
+			$reponse->execute(array($_POST['nouveau_mdp'],$_SESSION['membre_id']));
+			$reponse->execute();
+			echo "<p>Votre mot de passe a été modifié avec succès. Retour à <a href='index.php'>l'Accueil</a>.</p>";
+			}
+			else {
+				echo "Les 2 mots de passe ne correspondent pas.";
+			}
+		}
+		else {
+			echo "L'ancien mot de passe saisi n'est pas le bon.";
+		}
+	
+	?>
+
+
+<?php else: ?>
+<div class="form">
+	<form action="modifier_mdp.php" method="post">
+		<input type="password" name="motdepasse" id="motdepasse" placeholder="Ancien mot de passe" required><br/>
+		<input type="password" name="nouveau_mdp" id="nouveau_mdp" placeholder="Ton mot de passe" required><br/>
+		<input type="password" name="motdepasse_confirm" id="motdepasse_confirm" placeholder="Confirme ton mot de passe" required> <br>
+		<input type="submit" name="Modifier">
+	</form>
+</div>
+
+<?php endif; ?>
+<?php else: ?>
+	<p>Vous devez vous <a href="connexion.php">connecter</a> pour accéder à cette fonctionnalité. </p>
+<?php endif; ?>
+
+
 
 <?php include 'pied.php'; ?>
--- mon_compte.php
+++ mon_compte.php
@@ -3,15 +3,33 @@ $donnees['menu'] ='mon_compte';
 $donnees['titre_page']='Mon compte';
 include 'entete.php'; ?>
 
-<h1>Mon compte</h1>
+<h1>Mon compte</h1><br>
 
 <?php 
 	if(isset($_SESSION['membre_id']) AND $_SESSION['membre_id']>0){ ?>
+	<?php 
+	$requete = "SELECT membre.pseudo, membre.email  FROM membre WHERE membre.id = ?;";
+	$reponse=$pdo->prepare($requete);
+	$reponse->execute(array($_SESSION['membre_id']));
+	$enregistrements = $reponse->fetch();
+
+	$membre_pseudo = $enregistrements['pseudo'];
+	$membre_email = $enregistrements['email'];
+
+
+	?>
+
 		<ul>
-			<li>Mon pseudo :</li><br>
-			<li>Mon email : </li><br>
-			<li>Mon avatar : </li><br>
-			<li>Modifier mon mot de passe</li><br>
+			<li>Mon pseudo : <?php echo $membre_pseudo ?></li><br>
+			<li>Mon email : <?php echo $membre_email ?></li><br>
+		</ul>
+
+	
+	<form action="modifier_mdp.php" method="post">
+		<input type="hidden" name="id" value="<?= $_GET['id'] ?>" />
+		<input type="submit" value="Modifier mot de passe ">
+	</form>
+		
 	<?php
 	}
 	else{ ?>
