3b1cad10f28e93693c08561747ff3120cbdb0362
pplaut <38976707+pplaut@users.noreply.github.com>
2018-05-17 14:25:16 +0200

    Update a_propos.php

--- a_propos.php
+++ a_propos.php
@@ -19,16 +19,16 @@ include 'entete.php'; ?>
 
 	<h3>Les annonces</h3>
 	<p> 
-		"Let's Help : n'est-il pas temps de s'aider ?" est un projet universitaire crée par des étudiant-e-s de deuxième année de licence.<br/>
-		Le principe de ce site est de mettre en lien des personnes qui ont besoin d'aide et des qui acceptent de rendre service. <br/> 
-		Vous pouvez poster deux types d'annonce : Les types dits "demande", dans lequel vous pouvez formuler une demande d'aide, et le type dit "proposition" dans lequel vous présentez une de vos compétences, que vous accepteriez de mettre en oeuvre pour d'autres.
-		Pour poster une annonce, il vous suffit de lui donner un titre explicite, une description détaillée, sa catégorie, et dans la ville qui lui est associée. Le tout, respectant nos conditions d'utilisations, qui sont simplement du bon sens et du savoir vivre. (Celà me dédouane de les écrire) <br/>
-		Pour voir les annonces, il faut se rendre dans l'onglet "voir annonces". Par défaut, seront affichées des annonces des deux types (explicités plus haut), mais d'une couleur distinctes. Vous pourrez par la suite filtrer les annonces par type et catégorie.<br/><br/>
+		"Let's Help : n'est-il pas temps de s'aider ?" est un projet universitaire crée par des étudiant-e-s en deuxième année de licence.<br/>
+		Le principe de ce site est de mettre en lien des personnes qui ont besoin d'aide et des personnes qui acceptent de rendre service. <br/> 
+		Tu peux poster deux types d'annonce : Le type dit "demande", dans lequel tu peux formuler une demande d'aide, et le type dit "proposition" dans lequel tu présente une de tes compétences, que tu acceptes de mettre en oeuvre pour d'autres.
+		Pour poster une annonce, il te suffit de lui donner un titre explicite, une description détaillée, sa catégorie, et la ville qui lui est associée. Le tout, respectant nos conditions d'utilisations, qui sont simplement du bon sens et du savoir vivre. (Cela me dédouane de les écrire). <br/>
+		Pour voir les annonces, il faut se rendre dans l'onglet "voir annonces". Par défaut, seront affichées des annonces des deux types (explicités plus haut), mais de couleurs distinctes. Tu peux par la suite filtrer les annonces par type et catégorie.<br/><br/>
 		<h3>Cpasmafaute</h3>
-		Cpasmafaute est l'outil qui fait la spécifité et l'originalité de notre site. A l'origine, nous étions partis pour créer un site web uniquement dédié à cet outil, mais pour satisfaire les envies de chacuns des créateur-ice-s nous avons ajouté la possibilité de s'entraider par les annonces, ce qui finalement est une grosse proportion du site .. ;) </br>
-		Grâce à Cpasmafaute, vous pouvez demander des excuses adaptées à votre situation en cas de retard inexcusable. En effet, l'algorithme prend en paramètre votre votre statut (étudiant, prof, salarié..), votre temps de retard, la situation (travail, repas de famille, cours..) et vous propose une excuse adaptée. <br/>
-		Afin de nourrir cet outil, vous avez en tant qu'utilisateur inscrit, la possibilité de proposer une excuse, testée ou non, en indiquant le contexte dans lequel elle pourrait être utilisée adéquatement. <br/>
-		Lorsque vous avez utilisé une excuse, vous pourrez la retrouver dans votre compte, et ainsi la noter et la commenter une fois testée. Sympa non ? <br/>
+		Cpasmafaute est l'outil qui fait la spécifité et l'originalité de notre site. A l'origine, nous étions parti-e-s pour créer un site web uniquement dédié à cet outil, mais pour satisfaire les envies de chacun-e-s des créateur-ice-s nous avons ajouté la possibilité de s'entraider par les annonces, ce qui finalement est une grosse proportion du site .. ;) </br>
+		Grâce à Cpasmafaute, tu peux demander des excuses adaptées à ta situation en cas de retard inexcusable. En effet, l'algorithme prend en paramètre ton statut (étudiant-e, professeur-e, salarié-e..), ton temps de retard, la situation (travail, repas de famille, cours..) et te propose une excuse adaptée. <br/>
+		Afin de nourrir cet outil, tu as, en tant qu'utilisateur inscrit, la possibilité de proposer une excuse, testée ou non, en indiquant le contexte dans lequel elle pourrait être utilisée adéquatement. <br/>
+		Lorsque tu as utilisé une excuse, tu peux la retrouver dans ton compte, et ainsi la noter et la commenter une fois testée. Sympa non ? <br/>
 		Allez, amuse toi ;)
 		</p>
 </article>	
